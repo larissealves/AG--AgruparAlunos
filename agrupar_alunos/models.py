7896b941a2914aa8bff113ee5f1256018a540c8e
larissealves <alves.larisser@gmail.com>
2021-04-21 19:21:51 -0300

    Finish django configurations

---
+++ agrupar_alunos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Grupo(models.Model):
+    nome_grupo = models.CharField(max_length=200)
+    caracteristica_grupo = models.CharField(max_length=200)
+    limite_integrante_grupo = models.IntegerField()
+   
+    def __str__(self):
+        return self.nome_grupo 
+
+class Aluno(models.Model):
+    nome_aluno = models.CharField(max_length=200)
+    caracteristica_aluno = models.CharField(max_length=200)
+   
+    def __str__(self):
+        return self.nome_aluno + " - " + self.caracteristica_aluno
+
+
