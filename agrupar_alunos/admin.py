a97566dabfa3e09117e69f135a5315f171999a72
larissealves <alves.larisser@gmail.com>
2021-05-02 16:02:08 -0300

    Deploy Heroku

--- agrupar_alunos/admin.py
+++ agrupar_alunos/admin.py
@@ -7,7 +7,9 @@ from .models import Grupo, Aluno
 @admin.register(Aluno)
 class AlunoAdmin(admin.ModelAdmin):
     list_display = ("nome_aluno", "caracteristica_aluno")
-
+    
 @admin.register(Grupo)
 class AlunoAdmin(admin.ModelAdmin):
     list_display = ("nome_grupo", "caracteristica_grupo", "limite_integrante_grupo")
+    change_list_template = 'agrupar_alunos/link.html'
+
